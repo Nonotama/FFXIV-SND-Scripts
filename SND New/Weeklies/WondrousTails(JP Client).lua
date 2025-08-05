fff5c7e7e15183678007de9b9c216636bcda1bfc
tama Nono <nonotama0519@gmail.com>
2025-08-05 15:33:25 +0900

    パッチ7.3のレイド名に内容に更新

--- SND New/Weeklies/WondrousTails(JP Client).lua
+++ SND New/Weeklies/WondrousTails(JP Client).lua
@@ -146,8 +146,7 @@ local wonderousTailsDuties = {
             { dutyName="万魔殿パンデモニウム：辺獄編1-4", dutyId=1002 },
             { dutyName="万魔殿パンデモニウム：煉獄編1-4", dutyId=1081 },
             { dutyName="万魔殿パンデモニウム：天獄編1-4", dutyId=1147 },
-            { dutyName="至天の座アルカディア：ライトヘビー級1-2", dutyId=1125 },
-            { dutyName="至天の座アルカディア：ライトヘビー級3-4", dutyId=1231 },
+            { dutyName="至天の座アルカディア：ライトヘビー級1-4", dutyId=1125 },
             { dutyName="至天の座アルカディア：クルーザー級1-2", dutyId=1231 },
             { dutyName="至天の座アルカディア：クルーザー級3-4", dutyId=1231 },
         }
